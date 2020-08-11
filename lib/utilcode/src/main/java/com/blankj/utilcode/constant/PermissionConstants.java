836a7584584d4362df66ed12c4069721c3c37902
Lenovo C940 <475362194@qq.com>
2020-08-11 12:18:40 +0800

    权限请求适配安卓10的定位权限

--- lib/utilcode/src/main/java/com/blankj/utilcode/constant/PermissionConstants.java
+++ lib/utilcode/src/main/java/com/blankj/utilcode/constant/PermissionConstants.java
@@ -1,8 +1,10 @@
 package com.blankj.utilcode.constant;
 
+import android.Manifest;
 import android.Manifest.permission;
 import android.annotation.SuppressLint;
 import android.os.Build;
+
 import androidx.annotation.StringDef;
 
 import java.lang.annotation.Retention;
@@ -20,32 +22,35 @@ import java.lang.annotation.RetentionPolicy;
 @SuppressLint("InlinedApi")
 public final class PermissionConstants {
 
-    public static final String CALENDAR   = "CALENDAR";
-    public static final String CAMERA     = "CAMERA";
-    public static final String CONTACTS   = "CONTACTS";
-    public static final String LOCATION   = "LOCATION";
+    public static final String CALENDAR = "CALENDAR";
+    public static final String CAMERA = "CAMERA";
+    public static final String CONTACTS = "CONTACTS";
+    public static final String LOCATION = "LOCATION";
     public static final String MICROPHONE = "MICROPHONE";
-    public static final String PHONE      = "PHONE";
-    public static final String SENSORS    = "SENSORS";
-    public static final String SMS        = "SMS";
-    public static final String STORAGE    = "STORAGE";
+    public static final String PHONE = "PHONE";
+    public static final String SENSORS = "SENSORS";
+    public static final String SMS = "SMS";
+    public static final String STORAGE = "STORAGE";
 
-    private static final String[] GROUP_CALENDAR      = {
+    private static final String[] GROUP_CALENDAR = {
             permission.READ_CALENDAR, permission.WRITE_CALENDAR
     };
-    private static final String[] GROUP_CAMERA        = {
+    private static final String[] GROUP_CAMERA = {
             permission.CAMERA
     };
-    private static final String[] GROUP_CONTACTS      = {
+    private static final String[] GROUP_CONTACTS = {
             permission.READ_CONTACTS, permission.WRITE_CONTACTS, permission.GET_ACCOUNTS
     };
-    private static final String[] GROUP_LOCATION      = {
+    private static final String[] GROUP_LOCATION = {
             permission.ACCESS_FINE_LOCATION, permission.ACCESS_COARSE_LOCATION
     };
-    private static final String[] GROUP_MICROPHONE    = {
+    private static final String[] GROUP_LOCATION_WITH_BACKGROUND = {
+            permission.ACCESS_FINE_LOCATION, permission.ACCESS_COARSE_LOCATION, permission.ACCESS_BACKGROUND_LOCATION
+    };
+    private static final String[] GROUP_MICROPHONE = {
             permission.RECORD_AUDIO
     };
-    private static final String[] GROUP_PHONE         = {
+    private static final String[] GROUP_PHONE = {
             permission.READ_PHONE_STATE, permission.READ_PHONE_NUMBERS, permission.CALL_PHONE,
             permission.READ_CALL_LOG, permission.WRITE_CALL_LOG, permission.ADD_VOICEMAIL,
             permission.USE_SIP, permission.PROCESS_OUTGOING_CALLS, permission.ANSWER_PHONE_CALLS
@@ -55,14 +60,14 @@ public final class PermissionConstants {
             permission.READ_CALL_LOG, permission.WRITE_CALL_LOG, permission.ADD_VOICEMAIL,
             permission.USE_SIP, permission.PROCESS_OUTGOING_CALLS
     };
-    private static final String[] GROUP_SENSORS       = {
+    private static final String[] GROUP_SENSORS = {
             permission.BODY_SENSORS
     };
-    private static final String[] GROUP_SMS           = {
+    private static final String[] GROUP_SMS = {
             permission.SEND_SMS, permission.RECEIVE_SMS, permission.READ_SMS,
             permission.RECEIVE_WAP_PUSH, permission.RECEIVE_MMS,
     };
-    private static final String[] GROUP_STORAGE       = {
+    private static final String[] GROUP_STORAGE = {
             permission.READ_EXTERNAL_STORAGE, permission.WRITE_EXTERNAL_STORAGE,
     };
 
@@ -81,7 +86,10 @@ public final class PermissionConstants {
             case CONTACTS:
                 return GROUP_CONTACTS;
             case LOCATION:
-                return GROUP_LOCATION;
+                if (Build.VERSION.SDK_INT >= 29)
+                    return GROUP_LOCATION_WITH_BACKGROUND;
+                else
+                    return GROUP_LOCATION;
             case MICROPHONE:
                 return GROUP_MICROPHONE;
             case PHONE:
